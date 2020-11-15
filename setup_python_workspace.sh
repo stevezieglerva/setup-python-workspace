d13726bb38c4b2915316ef8a30183bfab49de002
Steve Ziegler <stephen.v.ziegler@gmail.com>
2020-11-15 10:13:14 -0500

    also copy run tests

--- setup_python_workspace.sh
+++ setup_python_workspace.sh
@@ -23,7 +23,7 @@ mkdir tests
 touch tests/test_unit.py
 curl -o test_and_format.py https://raw.githubusercontent.com/stevezieglerva/python-unittest-formatted-results/main/test_and_format.py
 curl -o report_template_json.txt https://raw.githubusercontent.com/stevezieglerva/python-unittest-formatted-results/main/report_template_json.txt
-curl -o report_template_json.txt https://raw.githubusercontent.com/stevezieglerva/python-unittest-formatted-results/main/test.sh
+curl -o run_tests.sh https://raw.githubusercontent.com/stevezieglerva/python-unittest-formatted-results/main/run_tests.sh
 
 
 code $curdir_only.code-workspace
